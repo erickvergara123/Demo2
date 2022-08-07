81adc533f3dd63ad3fca904cdec28b75c69bbd21
EIVERGARAO <erick.vergara@gmail.com>
2022-08-06 23:24:03 -0500

    Otro mas

--- src/main/java/sec/project/config/CustomUserDetailsService.java
+++ src/main/java/sec/project/config/CustomUserDetailsService.java
@@ -14,7 +14,7 @@ import sec.project.repository.UserRolesRepository;
 
 
 
-//Erick otro mas
+//Erick dos mas
 @Service("customUserDetailsService")
 public class CustomUserDetailsService implements UserDetailsService{
 	private final UserRepository userRepository;
